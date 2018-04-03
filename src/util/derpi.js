5c6d3cfccd73c48ac1ab69911ca1819f568dadcc
Eliot Partridge <shortcircuit369@gmail.com>
2018-04-02 19:04:28 -0500

    Clean up URL buliding, support filter IDs

--- src/util/derpi.js
+++ src/util/derpi.js
@@ -1,29 +1,41 @@
 const https = require('https');
 
+const defaultQueryOptions = {
+	query: '*',
+	order: 'desc',
+	sortFormat: 'created_at',
+	page: 1,
+	filter: 56027,
+};
+
 /**
- * Queries Derpibooru for images matching the given criteria 
- * 
+ * Queries Derpibooru for images matching the given criteria
+ *
  * @param {object} options - The set of options to use
  * @param {string} [options.apiKey] - Your Derpibooru API key
  * @param {string} [options.query="*"] - The query to send to Derpibooru
  * @param {string} [options.sortFormat="created_at"] - The sort format to order the results by
  * @param {string} [options.order="desc"] - The sort order (asc - ascending/desc - descending) for the result set
  * @param {number} [options.page=1] - The page of results to fetch
+ * @param {number} [options.filter=56027] - The filter ID to use
  * @param {function} callback - The callback to run with the result set or errors
  */
 exports.query = (options, callback) => {
-	let {apiKey, query, sortFormat, order, page} = options;
+	options = Object.assign({}, defaultQueryOptions, options);
 
-	// If the query is empty, we assume that that means search *everything*
-	if (!query) query = '*';
-	
-	if (!order) order = 'desc';
+	let url = 'https://derpibooru.org/search.json';
 
-	if (!sortFormat) sortFormat = 'created_at';
+	url += `?page=${options.page}`;
+	url += `&sf=${options.sortFormat}`;
+	url += `&sd=${options.order}`;
+	url += `&q=${encodeURIComponent(options.query)}`;
+	url += `&filter_id=${options.filter}`;
 
-	if (!page) page = 1;
+	if (options.apiKey) {
+		url += `&key=${options.apiKey}`;
+	}
 
-	https.get(`https://derpibooru.org/search.json?page=${page}&sf=${sortFormat}&sd=${order}&q=${encodeURIComponent(query)}${apiKey ? `&key=${apiKey}` : ''}`, (res) => {
+	https.get(url, (res) => {
 		const { statusCode } = res;
 		if (statusCode !== 200) {
 			res.resume();
@@ -32,7 +44,7 @@ exports.query = (options, callback) => {
 		}
 
 		res.setEncoding('utf8');
-		
+
 		let rawData = '';
 
 		res.on('data', (chunk) => { rawData += chunk; });
