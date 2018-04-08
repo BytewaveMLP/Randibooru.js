e34e1a7384ab05fdbbc4c8548303a7b442541ed0
Eliot Partridge <shortcircuit369@gmail.com>
2018-04-08 00:13:13 -0500

    Get *unique* members from guilds, not all members

--- src/util/helpers.js
+++ src/util/helpers.js
@@ -3,23 +3,31 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 /**
- * Gets the current guild and member counts.
+ * Gets the current guild and UNIQUE member counts.
  *
  * @param {CommandoClient} client - The Discord.JS-Commando Client object
  * @returns {object} - An object containing two properties, guilds and members, each representing the associated counts.
  */
 exports.getGuildsAndMembers = client => {
-	return client.guilds.reduce((acc, guild) => {
-		acc.guilds  += 1;
-		acc.members += guild.members.array().length;
-		return acc;
-	}, {
-		guilds:  0,
-		members: 0,
-	});
+	// Algorithm provided by CyberPon3
+	// https://github.com/CyberPon3
+	let members = client.guilds.reduce(
+		(acc, guild) => {
+			guild.members
+				.filter(member => !member.user.bot)
+				.forEach(member => acc.add(member.id));
+
+			return acc;
+		},
+		new Set()
+	).size;
+
+	let guilds = client.guilds.array().length;
+
+	return { members: members, guilds: guilds };
 };
 
 exports.setGame = client => {
-	let { guilds, members } = this.getGuildsAndMembers(client);
-	client.user.setGame(`${client.commandPrefix || client.options.commandPrefix}help | Serving ${members} members across ${guilds} guilds`);
+	let { members, guilds } = this.getGuildsAndMembers(client);
+	client.user.setGame(`${client.commandPrefix || client.options.commandPrefix}help | ${members} members across ${guilds} guilds`);
 };
